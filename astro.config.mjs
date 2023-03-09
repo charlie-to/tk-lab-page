2c2532489227f7bb529970301e5d2a1bead0eb65
tsubasa <tsubasa.otsuki.p2@dc.tohoku.ac.jp>
2023-03-09 11:54:30 +0900

    add astro-seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import image from '@astrojs/image'
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
+/** @type {import('astro').AstroUserConfig;} */
 export default defineConfig({
+  site: 'http://tohoku-takahashi.sakura.ne.jp',
   integrations: [tailwind(), image(), sitemap()]
 })
